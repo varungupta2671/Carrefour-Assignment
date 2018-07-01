978ba2739045c9a7dc84779358e6fb1f595925fe
Varun Gupta <varungupta2671@gmail.com>
2018-07-02 03:35:11 +0530

    service called

--- app/login/login.js
+++ app/login/login.js
@@ -9,8 +9,9 @@ angular.module('myApp.login', ['ngRoute'])
     });
 }])
 
-.controller('loginCtrl', ['$scope', function($scope) {
+.controller('loginCtrl', ['$scope', 'services', function($scope, services) {
 
+    $scope.user = [];
     // console.log("loginCtrl Calling ..");
     $scope.productList = [
         { "src": "./../img/logo@2X2.png", "price": "AED 1000", "description": "iPhone 6 Black" },
@@ -19,4 +20,25 @@ angular.module('myApp.login', ['ngRoute'])
         { "src": "./../img/logo@2X2.png", "price": "AED 4000", "description": "iPhone x Black" },
         { "src": "./../img/logo@2X2.png", "price": "AED 5000", "description": "iPhone 8+ Black" }
     ];
-}]);
+
+    $scope.submitForm = function() {
+        console.log("submitForm called..");
+
+        var data = $scope.user;
+        services.callLogin(function(data, response) {
+            console.log(response);
+        });
+    }
+}])
+
+.service('services', function($http) {
+
+    this.callLogin = function(loginData, done) {
+
+        console.log("service called..");
+        $http.post("test.html", { "username": loginData.email, "password": loginData.password })
+            .then(function(response) {
+                done(response.data);
+            });
+    };
+});
