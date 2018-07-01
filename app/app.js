f6247e412ac81e24228b4e98f8b2616a8825e646
Varun Gupta <varungupta2671@gmail.com>
2018-07-02 00:44:28 +0530

    coflict resolved

--- app/app.js
+++ app/app.js
@@ -3,10 +3,10 @@
 // Declare app level module which depends on views, and components
 angular.module('carrefourApp', [
     'ngRoute',
-    'myApp.view1'
+    'myApp.login'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
     $locationProvider.hashPrefix('!');
 
-    $routeProvider.otherwise({ redirectTo: '/view1' });
+    $routeProvider.otherwise({ redirectTo: '/login' });
 }]);
